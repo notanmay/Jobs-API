c84c96363030cd072b826ca7fe1aa6677ec5b97f
unknown <tejasdhamecha231@gmail.com>
2022-05-12 20:44:27 +0530

    updated get route

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// extra packages
+app.get('/', (req, res) => {
+  res.send('Jobs API');
+});
 
 // routes
 app.use('/api/v1/auth', authRouter)
